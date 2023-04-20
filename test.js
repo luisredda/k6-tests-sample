8ce0d300d7f62db7b7b73e441fb671927061d590
Luis Redda <luis.redda@gmail.com>
2023-04-19 21:25:33 -0300

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  const res = http.get(`${__ENV.MY_HOSTNAME}/`);
+  const res = http.get(`https://${__ENV.MY_HOSTNAME}/`);
   sleep(1);
 }
